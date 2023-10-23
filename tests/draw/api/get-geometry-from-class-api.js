98e19284775ee59e28ce1b95f6c9557d0449df1d
Fedor Kobyakov <fedor.kobyakov@onlyoffice.com>
2023-10-23 16:47:00 +0300

    [ve] Add NURBS draw using de Boor's algorithm

--- tests/draw/api/get-geometry-from-class-api.js
+++ tests/draw/api/get-geometry-from-class-api.js
@@ -507,6 +507,114 @@
 					}
 					case "NURBSTo":
 					{
+						// using https://github.com/thibauts/b-spline by thibauts under MIT Licence
+						// The MIT License (MIT)
+						//
+						// Copyright (c) 2015 Thibaut Séguy <thibaut.seguy@gmail.com>
+						//
+						// Permission is hereby granted, free of charge, to any person obtaining a copy
+						// of this software and associated documentation files (the "Software"), to deal
+						// in the Software without restriction, including without limitation the rights
+						// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+						// copies of the Software, and to permit persons to whom the Software is
+						// furnished to do so, subject to the following conditions:
+						//
+						// The above copyright notice and this permission notice shall be included in
+						// all copies or substantial portions of the Software.
+						//
+						// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+						// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+						// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+						// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+						// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+						// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+						// THE SOFTWARE.
+						function interpolate(t, degree, points, knots, weights, result) {
+
+							var i,j,s,l;              // function-scoped iteration variables
+							var n = points.length;    // points count
+							var d = points[0].length; // point dimensionality
+
+							if(degree < 1) throw new Error('degree must be at least 1 (linear)');
+							if(degree > (n-1)) throw new Error('degree must be less than or equal to point count - 1');
+
+							if(!weights) {
+								// build weight vector of length [n]
+								weights = [];
+								for(i=0; i<n; i++) {
+									weights[i] = 1;
+								}
+							}
+
+							if(!knots) {
+								// build knot vector of length [n + degree + 1]
+								var knots = [];
+								for(i=0; i<n+degree+1; i++) {
+									knots[i] = i;
+								}
+							} else {
+								if(knots.length !== n+degree+1) throw new Error('bad knot vector length');
+							}
+
+							var domain = [
+								degree,
+								knots.length-1 - degree
+							];
+
+							// remap t to the domain where the spline is defined
+							var low  = knots[domain[0]];
+							var high = knots[domain[1]];
+							t = t * (high - low) + low;
+
+							if(t < low || t > high) throw new Error('out of bounds');
+
+							// find s (the spline segment) for the [t] value provided
+							for(s=domain[0]; s<domain[1]; s++) {
+								if(t >= knots[s] && t <= knots[s+1]) {
+									break;
+								}
+							}
+
+							// convert points to homogeneous coordinates
+							var v = [];
+							for(i=0; i<n; i++) {
+								v[i] = [];
+								for(j=0; j<d; j++) {
+									v[i][j] = points[i][j] * weights[i];
+								}
+								v[i][d] = weights[i];
+							}
+
+							// l (level) goes from 1 to the curve degree + 1
+							var alpha;
+							for(l=1; l<=degree+1; l++) {
+								// build level l of the pyramid
+								for(i=s; i>s-degree-1+l; i--) {
+									alpha = (t - knots[i]) / (knots[i+degree+1-l] - knots[i]);
+
+									// interpolate each component
+									for(j=0; j<d+1; j++) {
+										v[i][j] = (1 - alpha) * v[i-1][j] + alpha * v[i][j];
+									}
+								}
+							}
+
+							// convert back to cartesian and return
+							var result = result || [];
+							for(i=0; i<d; i++) {
+								result[i] = v[s][i] / v[s][d];
+							}
+
+							return result;
+						}
+
+						//TODO
+						// check relative of what to calculate xScale and yScale maybe it is not shapeWidth and shapeHeight
+						// get to know what is xType and yType
+						// check non 3-degrees NURBS
+						// consider homogenous or euclidian points weights
+						// consider NURBS length setting points count
+
 						// https://learn.microsoft.com/en-us/office/client-developer/visio/nurbsto-row-geometry-section
 						let xEndPoint = Number(findCell(commandRow, "n", "X").v);
 						let yEndPoint = Number(findCell(commandRow, "n", "Y").v);
@@ -567,13 +675,36 @@
 						knots.push(preLastKnot);
 						knots.push(lastKnot);
 						// add 3 more knots for 3 degree NURBS to clamp curve at end point
+						// a clamped knot vector must have `degree + 1` equal knots
 						for (let j = 0; j < degree; j++) {
 							knots.push(lastKnot);
 						}
 						weights.push(lastWeight);
 						controlPoints.push({x: xEndPoint, y: yEndPoint});
 
-						console.log("NURBSTo command draw is not realized");
+						// Drawing commands create
+						let pointsCount = 1000;
+						let controlPointsArrayFormat = controlPoints.map(function (point) {
+							return [point.x, point.y];
+						});
+						let prevX = convertUnits(prevLastX, additionalUnitCoefficient);
+						let prevY = convertUnits(prevLastY, additionalUnitCoefficient);
+						for(let t = 0; t < 1; t += 1/pointsCount) {
+							let point;
+							try {
+								point = interpolate(t, degree, controlPointsArrayFormat, knots, weights);
+							} catch (e) {
+								console.log(e);
+								break;
+							}
+							let newX = convertUnits(point[0], additionalUnitCoefficient);
+							let newY = convertUnits(point[1], additionalUnitCoefficient);
+							path.lnTo(newX, newY);
+							prevX = newX;
+							prevY = newY;
+						}
+						lastPoint.x = prevX;
+						lastPoint.y = prevY;
 						break;
 					}
 					case "SplineStart":
