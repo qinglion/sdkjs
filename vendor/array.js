f96312cf1051d1bafdbc01018d35a3caaf3df4f8
Fedor Kobyakov <fedor.kobyakov@onlyoffice.com>
2023-12-07 12:23:46 +0300

    Add Array.find polyfill

--- vendor/array.js
+++ vendor/array.js
@@ -26,6 +26,28 @@
 		});
 	}
 
+	if (!Array.prototype.find)
+	{
+		Object.defineProperty(Array.prototype, 'find', {
+			value: function (predicate) {
+				if (this === null) {
+					throw new TypeError('Array.prototype.find called on null or undefined');
+				} else if (typeof predicate !== 'function') {
+					throw new TypeError('predicate must be a function');
+				}
+				var list = Object(this);
+				var length = list.length >>> 0;
+				var thisArg = arguments[1];
+				for (var i = 0; i < length; i++) {
+					var element = list[i];
+					if ( predicate.call(thisArg, element, i, list) ) {
+						return element;
+					}
+				}
+			}
+		});
+	}
+
 	var typed_arrays = [
 		Array,
 		Int8Array,
