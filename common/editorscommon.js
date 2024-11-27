a0562baa3d53942f81e8ed54282c07a8fa4c33a3
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2024-11-27 15:19:54 +0300

    [all] Move registerServiceWorker to web-apps

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -14768,31 +14768,6 @@
 		return aArray[Math.random() * aArray.length | 0];
 	}
 
-	function registerServiceWorker() {
-		if ('serviceWorker' in navigator) {
-			const serviceWorkerName = 'document_editor_service_worker.js';
-			const serviceWorkerPath = '../../../../' + serviceWorkerName;
-			let reg;
-			navigator.serviceWorker.register(serviceWorkerPath)
-				.then(function (registration) {
-					reg = registration;
-					return navigator.serviceWorker.getRegistrations();
-				})
-				.then(function (registrations) {
-					//delete stale service workers
-					for (const registration of registrations) {
-						if (registration !== reg && registration.active && registration.active.scriptURL.endsWith(serviceWorkerName)) {
-							registration.unregister();
-						}
-					}
-				})
-				.catch(function (err) {
-					console.error('Registration failed with ' + err);
-				});
-		}
-	}
-	registerServiceWorker();
-
 	function consoleLog(val) {
 		//console.log(val);
 	}
