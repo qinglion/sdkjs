26cbdc0c18e0a131d3f8fa1a6006628f4bf963ae
GoshaZotov <Igor.Zotov@onlyoffice.com>
2024-12-20 14:38:25 +0300

    [se] Fix bug 72203

--- cell/view/CellEditorView.js
+++ cell/view/CellEditorView.js
@@ -705,11 +705,11 @@ function (window, undefined) {
 
 				// ToDo move this code to moveCursor
 
-				this.lastRangePos = this._parseResult && this._parseResult.argPosArr
+				this.lastRangePos = this._parseResult && this._parseResult.argPosArr && this._parseResult.argPosArr.length
 					? this._parseResult.argPosArr[0].start 
 					: this.cursorPos;
 
-				this.lastRangeLength = this._parseResult && this._parseResult.argPosArr
+				this.lastRangeLength = this._parseResult && this._parseResult.argPosArr && this._parseResult.argPosArr.length
 					? this._parseResult.argPosArr[this._parseResult.argPosArr.length - 1].end - this._parseResult.argPosArr[0].start 
 					: 0;
 			}
