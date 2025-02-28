f978e5d233dc77cf01022ca465e44713e5716c9c
Ilya Kirillov <ilya.kirillov@onlyoffice.com>
2025-02-28 18:30:27 +0300

    [oform] Fix minimization

--- word/api.js
+++ word/api.js
@@ -11412,7 +11412,7 @@ background-repeat: no-repeat;\
 			return false;
 		
 		logicDocument.StartAction(AscDFH.historydescription_DisconnectEveryone);
-		logicDocument.GetHistory().Add(new CChangesDocumentDisconnectEveryone(logicDocument));
+		logicDocument.GetHistory().Add(new AscDFH.CChangesDocumentDisconnectEveryone(logicDocument));
 		logicDocument.FinalizeAction();
 		
 		this.forceSaveDisconnectRequest = true;
@@ -11431,7 +11431,7 @@ background-repeat: no-repeat;\
 		logicDocument.StartAction(AscDFH.historydescription_OForm_CompletePreparation);
 		logicDocument.GetOFormDocument().setAllRolesNotFilled();
 		if (disconnect)
-			logicDocument.GetHistory().Add(new CChangesDocumentDisconnectEveryone(logicDocument));
+			logicDocument.GetHistory().Add(new AscDFH.CChangesDocumentDisconnectEveryone(logicDocument));
 		
 		logicDocument.FinalizeAction();
 		
