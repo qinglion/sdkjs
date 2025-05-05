a3c970bd0069fb9efefe96813bfe34fb37ffad90
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2025-05-05 13:45:52 +0300

    do not draw paramarks in text arts with transform

--- common/Drawings/Format/Shape.js
+++ common/Drawings/Format/Shape.js
@@ -5517,9 +5517,10 @@
 				} else {
 					var oTheme = this.getParentObjects().theme;
 					var oColorMap = this.Get_ColorMap();
+					var bEditTextArt = isRealObject(oController) && (AscFormat.getTargetTextObject(oController) === this);
 					if (!this.bWordShape && (!this.txBody.content || this.txBody.content.Is_Empty()) && !AscCommon.IsShapeToImageConverter && this.txBody.content2 != null && !this.txBody.checkCurrentPlaceholder() && (this.isEmptyPlaceholder ? this.isEmptyPlaceholder() : false)) {
 						if (graphics.IsNoDrawingEmptyPlaceholder !== true && graphics.IsNoDrawingEmptyPlaceholderText !== true && !graphics.isPdf()) {
-							if (editor && editor.ShowParaMarks) {
+							if (isShowParaMarksInTextArt(this, bEditTextArt)) {
 								this.txWarpStructParamarks2.draw(graphics, this.transformTextWordArt2, oTheme, oColorMap);
 							} else {
 								if (this.txWarpStruct2)
@@ -5532,7 +5533,6 @@
 						var result_page_index = AscFormat.isRealNumber(graphics.shapePageIndex) ? graphics.shapePageIndex : (oContent ? oContent.Get_StartPage_Relative() : 0);
 						graphics.PageNum = result_page_index;
 						var bNeedRestoreState = false;
-						var bEditTextArt = isRealObject(oController) && (AscFormat.getTargetTextObject(oController) === this);
 						if (this.bWordShape && this.clipRect /*&& (!this.bodyPr.prstTxWarp || this.bodyPr.prstTxWarp.preset === "textNoShape" || bEditTextArt)*/) {
 							bNeedRestoreState = true;
 							var clip_rect = this.clipRect;
@@ -5550,7 +5550,7 @@
 						}
 
 						var oTransform = this.transformTextWordArt;
-						if (editor && editor.ShowParaMarks) {
+						if (isShowParaMarksInTextArt(this, bEditTextArt)) {
 							if (bEditTextArt && this.txWarpStructParamarksNoTransform) {
 								this.txWarpStructParamarksNoTransform.draw(graphics, this.transformText, oTheme, oColorMap);
 							} else if (this.txWarpStructParamarks) {
@@ -7446,6 +7446,13 @@
 					AscFonts.FontPickerByCharacter.getFontsByString(AscCommon.translateManager.getValue(AscFormat.pHText[i]));
 			}
 		};
+
+		function isShowParaMarksInTextArt(shape, bCurrentEditText) {
+			if(!Asc.editor.ShowParaMarks) return false;
+			if(bCurrentEditText) return true;
+			if(shape.chekBodyPrTransform(shape.getBodyPr())) return false;
+			return true;
+		}
 		//--------------------------------------------------------export----------------------------------------------------
 		window['AscFormat'] = window['AscFormat'] || {};
 		window['AscFormat'].CheckObjectLine = CheckObjectLine;
