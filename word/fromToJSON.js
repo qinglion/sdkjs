e94bc1b8d4220696823059fb21e4d8ded2a5f0ba
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2025-05-12 14:17:44 +0700

    [bu] Fix From/To json methods

--- word/fromToJSON.js
+++ word/fromToJSON.js
@@ -466,14 +466,14 @@
 			oResultObj = this.SerImage(oGraphicObj);
 		else if (oGraphicObj instanceof AscFormat.CShape)
 		{
-			if (oGraphicObj.constructor.name === "CConnectionShape")
+			if (oGraphicObj.getObjectType() === AscDFH.historyitem_type_Cnx)
 			{
 				var oShapeObj = this.SerShape(oGraphicObj);
 				oShapeObj["type"] = "connectShape";
 
 				oResultObj = oShapeObj;
 			}
-			else if (oGraphicObj.constructor.name === "CSlicer")
+			else if (oGraphicObj.getObjectType() === AscDFH.historyitem_type_SlicerView)
 				oResultObj = this.SerSlicerDrawing(oGraphicObj)
 			else
 				oResultObj = this.SerShape(oGraphicObj);
@@ -484,8 +484,10 @@
 			oResultObj = this.SerSmartArt(oGraphicObj);
 		else if (oGraphicObj instanceof AscFormat.CLockedCanvas)
 			oResultObj = this.SerLockedCanvas(oGraphicObj);
-		else if (oGraphicObj instanceof AscFormat.CGroupShape && oGraphicObj.constructor.name === "CGroupShape")
+		else if (oGraphicObj instanceof AscFormat.CGroupShape && oGraphicObj.getObjectType() === AscDFH.historyitem_type_GroupShape)
 			oResultObj = this.SerGroupShape(oGraphicObj);
+		else
+			return undefined;
 		
 		oResultObj["id"] = oGraphicObj.Id;
 
@@ -7939,7 +7941,7 @@
 			var oTempElm   = null;
 			var arrContent = [];
 
-			if (oMathContent.constructor.name === "CDenominator" || oMathContent.constructor.name === "CNumerator")
+			if (oMathContent instanceof AscCommonWord.CDenominator || oMathContent instanceof AscCommonWord.CNumerator)
 				arrContent.push(SerFracArg.call(this, oMathContent));
 			else 
 			{
@@ -8023,9 +8025,9 @@
 				return oFractionArg;
 
 			var sArgType = "";
-			if (oFractionArg.constructor.name === "CDenominator")
+			if (oFractionArg instanceof AscCommonWord.CDenominator)
 				sArgType = "den";
-			else if (oFractionArg.constructor.name === "CNumerator")
+			else if (oFractionArg instanceof AscCommonWord.CNumerator)
 				sArgType = "num";
 
 			return {
